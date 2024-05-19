fac5b0774493f03a49007603db65206c35f31408
thada.nguyen <thada.nguyen.1502@gmail.com>
2024-05-19 20:55:25 +0700

    updating all codes generated

---
+++ ThanhDatLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DummyLibrary
+{
+    public class Class1
+    {
+        public string GetMessage()
+        {
+            return "Hello from DummyLibrary by Thanh Dat!";
+        }
+    }
+}
---
+++ ThanhDatLib2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhDatLib2
+{
+    public class DummyClasses
+    {
+        public string GetMessage()
+        {
+            return "Hello from DummyLibrary 2 by Thanh Dat!";
+        }
+    }
+}
